dac5f4c9e86981d328b60d862b7215d8a8236e70
xobotyi <xog3@yandex.ru>
2018-03-15 16:08:48 +0300

    Due to necessity to write tests - removing root namespace qualifier;

--- src/SocketFunctions.php
+++ src/SocketFunctions.php
@@ -12,54 +12,54 @@
     {
         public
         function fsockopen(string $hostName, int $port = -1, &$errNo = null, &$errStr = null, int $timeout = null) {
-            return @\fsockopen($hostName, $port, $errNo, $errStr, $timeout);
+            return @fsockopen($hostName, $port, $errNo, $errStr, $timeout);
         }
 
         public
         function pfsockopen(string $hostName, int $port = -1, &$errNo = null, &$errStr = null, int $timeout = null) {
-            return @\pfsockopen($hostName, $port, $errNo, $errStr, $timeout);
+            return @pfsockopen($hostName, $port, $errNo, $errStr, $timeout);
         }
 
         public
         function fopen($filename, $mode) {
-            return \fopen($filename, $mode);
+            return fopen($filename, $mode);
         }
 
         public
         function fclose($stream) :bool {
-            return \fclose($stream);
+            return fclose($stream);
         }
 
         public
         function feof($stream) :bool {
-            return \feof($stream);
+            return feof($stream);
         }
 
         public
         function fgets($stream, int $length = null) {
             if ($length) {
-                return \fgets($stream, $length);
+                return fgets($stream, $length);
             }
 
-            return \fgets($stream);
+            return fgets($stream);
         }
 
         public
         function fread($stream, int $length) {
-            return \fread($stream, $length);
+            return fread($stream, $length);
         }
 
         public
         function fwrite($stream, string $text, int $length = null) {
             if ($length) {
-                return \fwrite($stream, $text, $length);
+                return fwrite($stream, $text, $length);
             }
 
-            return \fwrite($stream, $text);
+            return fwrite($stream, $text);
         }
 
         public
         function setReadTimeout($stream, int $seconds, int $microseconds = 0) {
-            return \stream_set_timeout($stream, $seconds, $microseconds);
+            return stream_set_timeout($stream, $seconds, $microseconds);
         }
     }
