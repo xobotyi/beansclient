5cf523158f65a26df4205c2bfb4da1481172528b
xobotyi <xog3@yandex.ru>
2019-06-14 11:43:01 +0300

    Kick* commands;

---
+++ src/Command/KickJobCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace xobotyi\beansclient\Command;
+
+
+use xobotyi\beansclient\Command;
+use xobotyi\beansclient\Exception\CommandException;
+use xobotyi\beansclient\Interfaces\CommandInterface;
+use xobotyi\beansclient\Response;
+
+class KickJobCommand extends Command implements CommandInterface
+{
+    public
+    function __construct(int $jobId) {
+        if ($jobId <= 0) {
+            throw new CommandException('Job id must be a positive integer');
+        }
+
+        parent::__construct(CommandInterface::KICK_JOB, null, [$jobId]);
+    }
+
+    public
+    function processResponse(array $responseHeader, ?string $responseBody = null): bool {
+        if ($responseHeader[0] === Response::KICKED) {
+            return true;
+        }
+
+        if ($responseHeader[0] === Response::NOT_FOUND) {
+            return false;
+        }
+
+        throw new CommandException(sprintf('Got unexpected status code `%s`', $responseHeader[0]));
+    }
+}
